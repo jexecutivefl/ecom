287e08bdb633adeb5f8b3d11b058004eb2f88326
jexecutivefl <j.executivefl@gmail.com>
2019-12-30 08:03:59 -0500

    Adding validation steps. Closes #1. Closes #2. Closes #3.

--- index.js
+++ index.js
@@ -1,49 +1,15 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
+const authRouter = require('./routes/admin/auth');
 
-
-const usersRepo = require('./repositories/users');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
     keys: ['a98dfa6df09sdaf098sadf']
 }));
-
-app.get('/', (req, res)=> {
-   res.send(`
-    <div>
-    'Your id is: ' + ${req.session.userId}
-        <form action="" method="POST">
-            <input type="email" name="email" placeholder="Email" />
-            <input type="password" name="password" placeholder="Password" />
-            <input type="password" name="passwordConfirmation" placeholder="Password Confirmation">
-            <button>Sign Up</button>
-        </form>
-    </div>
-   `);
-});
-
-app.post('/', async (req, res)=>{
-    //deconstruct email
-    const {email, password, passwordConfirmation} = req.body;
-    //is email already in system
-    const existingUser = await usersRepo.getOneBy({email});
-    if (existingUser) {
-        return res.send('Email in use');
-    }
-    //Does the password and password confirmation match
-    if (password !== passwordConfirmation) {
-        return res.send('passwords must match');
-    }
-    //create account user and pers
-    const user = await usersRepo.create({email, password});
-    //store the id of that user inside the users cookie
-    req.session.userId = user.id;
-    //
-    res.send('account created');
-});
+app.use(authRouter);
 
 app.listen(3000, () => {
     console.log('Listening');
--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
     constructor(filename) {
@@ -29,16 +32,31 @@ class UsersRepository {
 
     }
     async create(attrs){
+        // attrs === {email: '', passord: ''}
         attrs.id = this.randomId();
+        const salt = crypto.randomBytes(8).toString('hex');
+        const buf = await scrypt(attrs.password, salt, 64);
         //todo: add created date and changed date
         // {email: 'asdfsdaf@gmail.com', password: 'asdfasdfasdf'}
         const records = await this.getAll();
-        records.push(attrs);
+        const record = {
+            ...attrs,
+            password: `${buf.toString('hex')}.${salt}`
+        }
+        records.push(record);
         // write the updated records array back to this.filename
         await this.writeAll(records);
 
         //return attributes
-        return attrs;
+        return record;
+    }
+    async comparePasswords(saved, supplied) {
+        // Saved -> password saved in our database. 'hashed.salt
+        // Supplied -> password given to us by a user trying to sign in
+        const [hashed, salt] = saved.split('.');
+        const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+        return hashed === hashedSuppliedBuf.toString('hex');
     }
     async writeAll(records) {
         //todo: change null to custom function that checks and changes the string
